3eb5a3a7201f853abd6ae206cf1e3113537d81d1
s0rensen <jobb@s0rensen.no>
2018-09-24 14:10:52 +0200

    Added large logo and slogan

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,14 +1,20 @@
 <template>
+    <main class="home">
+        <div class="header">
+            <h1 class="bigLogo">Drovee</h1>
+            <h2 class="slogan">Food on the fly.</h2>
+        </div>
+    </main>
 </template>
 
 <script>
 export default {
-    
 }
 </script>
 
 <style lang="scss">
-@import '../css/home.scss'
+@import '../css/config';
+@import '../css/home.scss';
 </style>
 
 
